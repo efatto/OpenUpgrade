c0ad51fe2cc1c9d215d0d1ae86765d2c4a896f52
sergiocorato <sergiocorato@gmail.com>
2026-06-15 12:23:16 +0200

    [FIX] lift constraints on hr_job no_of_hired_employee

--- openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/15.0.1.1/pre-migration.py
@@ -3,6 +3,7 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.lift_constraints(env.cr, "hr_job", "no_of_hired_employee")
     openupgrade.set_xml_ids_noupdate_value(
         env, "hr_recruitment", ["applicant_hired_template"], True
     )
