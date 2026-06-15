e6b46fa7f6a16745693e574a60d0c068d31d08c7
sergiocorato <sergiocorato@gmail.com>
2026-06-15 17:32:38 +0200

    [FIX] ensure utm_source name uniqueness

--- openupgrade_scripts/scripts/utm/16.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/utm/16.0.1.1/pre-migration.py
@@ -6,3 +6,16 @@ def migrate(env, version):
     # pre-migration of module base converted name column from utm_campaign
     # into jsonb, hence it suffices to copy the name column into title.
     openupgrade.copy_columns(env.cr, {"utm_campaign": [("name", "title", "jsonb")]})
+    # ensure utm_source_unique_name (name is already required)
+    langs = env["res.lang"].search([])
+    for lang in langs:
+        lang_code = lang.code
+        env.cr.execute(
+            f"""
+                UPDATE utm_source SET name = name || jsonb_build_object(
+                    '{lang_code}', (name->>'{lang_code}') || '_' || id::text
+                ) WHERE id NOT IN (
+                    SELECT MIN(id) FROM utm_source GROUP BY name->>'{lang_code}'
+                ) AND name ? '{lang_code}'
+            """
+        )
